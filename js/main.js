c768e98fde31581ec6e388ecc2ce84d3c4a3d159
comemaino <mat.maino@gmail.com>
2022-05-04 11:36:54 +0200

    Add: bonus 2 e 3

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const posts = [
     media: "https://unsplash.it/600/300?image=171",
     author: {
       name: "Phil Mangione",
-      image: "https://unsplash.it/300/300?image=15",
+      // image: "https://unsplash.it/300/300?image=15",
     },
     likes: 60,
     created: "2021-06-25",
@@ -49,17 +49,13 @@ posts.forEach((post) => {
 });
 
 const likedPosts = [];
-
 const likeBtns = document.querySelectorAll(".js-like-button");
-console.log(likeBtns);
+
 likeBtns.forEach((button, index) => {
   button.addEventListener("click", function (event) {
     event.preventDefault();
     console.log("clicked");
-    //cambiare il colore al bottone
-    this.classList.add("like-button--liked");
-    //incrementare il counter
-    // prelevo post clickato dall'array di oggetti tramite index del bottone nell'array
+
     const clickedPost = posts[index];
     // prelevo id oggetto clickato
     const clickedPostId = clickedPost.id;
@@ -69,16 +65,36 @@ likeBtns.forEach((button, index) => {
     );
     // da questo elemento prelevo numero likes e trasformo in integer
     let likesNumber = parseInt(likeCounter.textContent);
-    // incremento numero likes
-    likesNumber = likesNumber + 1;
-    // riscrivo contenuto elemento html
+    // se il post clickato non è presente nell'array ->
+    if (!likedPosts.includes(clickedPostId)) {
+      //cambiare il colore al bottone
+      this.classList.add("like-button--liked");
+      //incrementare il counter
+      // prelevo post clickato dall'array di oggetti tramite index del bottone nell'array
+      // incremento numero likes
+      likesNumber = likesNumber + 1;
+      // riscrivo contenuto elemento html
+
+      // salvo posts likati in nouvo array
+      likedPosts.push(clickedPostId);
+    } else {
+      console.log("togliere il like");
+      // togliere colore dal bottone
+      this.classList.remove("like-button--liked");
+      //decrementare il numero di likes
+      likesNumber = likesNumber - 1;
+
+      //togliere l'id del post dall'array likedPosts
+      const idIndexInLikedPosts = likedPosts.indexOf(clickedPostId);
+      likedPosts.splice(idIndexInLikedPosts, 1);
+      console.log(likedPosts);
+    }
     likeCounter.innerHTML = likesNumber;
     // salvo nuovo numeri di likes nell'array
     clickedPost.likes = likesNumber;
-
-    // salvo posts likati in nouvo array
-    likedPosts.push(clickedPostId);
     console.log(likedPosts);
+
+    // altrimenti
   });
 });
 
@@ -96,11 +112,12 @@ function createPostElement(postObject) {
   <div class="post__header">
     <div class="post-meta">
       <div class="post-meta__icon">
-        <img
-          class="profile-pic"
-          src="${author.image}"
-          alt="${author.name}"
-        />
+      ${
+        author.image
+          ? createAuthorImage(author)
+          : createPlaceholder(author.name)
+      }
+        
       </div>
       <div class="post-meta__data">
         <div class="post-meta__author">${author.name}</div>
@@ -151,4 +168,44 @@ function formatDate(originalDate) {
   return itDateString;
 }
 
-formatDate("2022-05-04");
+// gestire assenza immagine profilo con iniziali
+
+// restituisce elemento immagine
+/**
+ * restituisce elemento immagine
+ * @param {object} authorObject -> oggeto con dati autore
+ * @returns {string} -> template string immagine autore
+ */
+function createAuthorImage(authorObject) {
+  const { image, name } = authorObject;
+  const authorImage = `<img
+  class="profile-pic"
+  src="${image}"
+  alt="${name}"
+/>`;
+  return authorImage;
+}
+
+//restituisce elemento placeholder
+function createPlaceholder(authorName) {
+  //otteniamo stringa iniziali autore
+  const nameParts = authorName.split(" ");
+  console.log(nameParts);
+  //prepariamo variabile contenente inizili
+  let initials = "";
+  //per ogni elemento dell'array
+  // - prendiamo prima lettera e concateniamo alla variabile di iniziali
+  nameParts.forEach((name) => {
+    const firstLetter = name[0];
+    initials += firstLetter;
+  });
+  console.log(initials);
+  //creiamo stringa elemento dom
+  const placeholder = `
+  <div class="profile-pic-default">
+  <span>${initials}</span>`;
+
+  return placeholder;
+}
+
+createPlaceholder("Phil Mangione");
